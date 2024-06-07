d86a62ef455849f4118f972a3f11ffeb35608ad6
2oothpick <kobbyboateng14@gmail.com>
2024-06-07 23:05:20 +0000

    Add successful solution to problem

--- 035-search_insert_position/search_insert_position.js
+++ 035-search_insert_position/search_insert_position.js
@@ -1,9 +1,12 @@
 var searchInsert = function (nums, target) {
-    if (target === 0) {
-        return 0
-    }
     if (!nums.includes(target)) {
+        if (target < 0) {
+            return 0
+        }
         for (i = 0; i <= nums.length; i++) {
+            if (target === 0 && nums.at(0) >= 0) {
+                return 0
+            }
             if (target > nums[i] && target < nums[i + 1]) {
                 return (i + 1)
             } else if (target > (nums[i - 1] || 0) && target < nums[i + 1]) {
@@ -19,5 +22,3 @@ var searchInsert = function (nums, target) {
     }
 
 };
-
-console.log(searchInsert([-1, 3, 5, 6], 0))
