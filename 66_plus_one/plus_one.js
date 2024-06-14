061c16d06d26e9d82e3bb57a623edb8586b4d176
2oothpick <kobbyboateng14@gmail.com>
2024-06-14 22:29:26 +0000

    Final js solution to problem JS numbers lose acuracy past 15 digits

--- 66_plus_one/plus_one.js
+++ 66_plus_one/plus_one.js
@@ -1,5 +1,5 @@
-let input = [1,2,3]
-input = Number(input.toString().replace(/,/g, '')) + 1
+let input = [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]
+input = BigInt(input.toString().replace(/,/g, '')) + 1n
 answer = []
 for (item of String(input)){
     answer.push(Number(item))
